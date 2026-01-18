78c98d74eb84d321cede8e157c46cf6e47b281cf
siby369 <sibyraj369@gmail.com>
2026-01-18 09:38:00 +0530

    fix: add typescript typings to Avatar component

--- src/components/siby-ui/Avatar.tsx
+++ src/components/siby-ui/Avatar.tsx
@@ -1,2 +1,2 @@
-export const Avatar = ({ src, alt }) => <div className="size-10 overflow-hidden rounded-full"><img src={src} alt={alt} /></div>;
+export const Avatar = ({ src, alt }: { src: string; alt: string }) => <div className="size-10 overflow-hidden rounded-full"><img src={src} alt={alt} /></div>;
 // Updated: 2026-01-17 07:56:52
